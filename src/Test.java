4442462bcbf1bdc524e7b091f80920a9a0cea79e
MatisC <matis.chastin3@etu.univ-lorraine.fr>
2020-03-29 18:35:51 +0200

    add test

--- src/Test.java
+++ src/Test.java
@@ -13,7 +13,7 @@ public class Test {
         assertEquals("La taille devrait etre 7", 7, g1.nbColonne());
         assertEquals("La taille devrait etre 10", 10, g2.nbColonne());
     }
-    
+
     public void test_GrilleMoyenne(){
         Grille g1 = new Grille();
         g1.ajouter(1, 1);
@@ -45,6 +45,12 @@ public class Test {
     public void test_CreationJoueur(){
         Joueur j = new Joueur(1);
         assertEquals("Le joueur devrait etre le numero 1", 1, j.getNumero());
+    }
+
+    public void test_CreationJeu(){
+        Jeu j = new Jeu(11,3);
+        assertEquals("Le nombre de joueur doit etre de 3", 3, j.getLJoueurs().size());
+        assertEquals("Le nombre de colonne doit etre de 11", 11, j.getGrille().nbColonne());
     }
 
     public static void main(String[] args) {
